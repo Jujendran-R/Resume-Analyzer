3bf53af013962aac03f53c2960927487ebdba677
Jujendran-R <jujendran@gmail.com>
2026-03-16 10:02:12 +0530

    fix: removed port detection

--- app.py
+++ app.py
@@ -564,5 +564,5 @@ def analyze():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    # port = int(os.environ.ge)
+    app.run(host="0.0.0.0")
